0dcb9028d19a89be201a20ff9352873a35e3f112
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-06-14 00:11:35 +0800

    feat: Products/Index page

---
+++ resources/js/Pages/Products/Index.jsx
@@ -0,0 +1,12 @@
+import Layout from "@/Layouts/Layout"
+import { Head } from "@inertiajs/react"
+
+const Index = () => {
+  return (
+    <Layout>
+      <Head title="Home" />
+    </Layout>
+  )
+}
+
+export default Index
