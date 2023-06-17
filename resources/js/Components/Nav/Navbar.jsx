bd642d13b2910a0c580046140d016ae786b31e3e
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-06-17 17:32:15 +0800

    refactor: icons on Navbar

--- resources/js/Components/Nav/Navbar.jsx
+++ resources/js/Components/Nav/Navbar.jsx
@@ -15,92 +15,92 @@ import {
 } from "@material-tailwind/react";
 import {
   ChevronDownIcon,
-  UserCircleIcon,
-  CubeTransparentIcon,
+  HomeIcon,
   Bars3Icon,
   XMarkIcon,
-  FlagIcon,
-  ChatBubbleOvalLeftIcon,
-  UsersIcon,
-  FolderIcon,
-  Square3Stack3DIcon,
-  RocketLaunchIcon,
-  FaceSmileIcon,
-  PuzzlePieceIcon,
-  GiftIcon,
+  CubeIcon,
   CpuChipIcon,
 } from "@heroicons/react/24/outline";
+import { AiOutlineLaptop, AiOutlineDesktop } from "react-icons/ai"
+import CpuIcon from "../Icons/CpuIcon";
+import MotherboardIcon from "../Icons/MotherboardIcon";
+import GpuIcon from "../Icons/GpuIcon";
+import RamIcon from "../Icons/RamIcon";
+import HardDriveIcon from "../Icons/HardDriveIcon";
+import DisplayIcon from "../Icons/DisplayIcon";
+import CaseIcon from "../Icons/CaseIcon";
+import FanIcon from "../Icons/FanIcon";
  
 const colors = {
-  blue: "bg-blue-50 text-blue-500",
-  orange: "bg-orange-50 text-orange-500",
-  green: "bg-green-50 text-green-500",
-  "blue-gray": "bg-blue-gray-50 text-blue-gray-500",
-  purple: "bg-purple-50 text-purple-500",
-  teal: "bg-teal-50 text-teal-500",
-  cyan: "bg-cyan-50 text-cyan-500",
-  pink: "bg-pink-50 text-pink-500",
+  blue: "bg-blue-50 text-blue-500 fill-blue-500 stroke-blue-500",
+  orange: "bg-orange-50 text-orange-500 fill-orange-500 stroke-orange-500",
+  green: "bg-green-50 text-green-500 fill-green-500 stroke-green-500",
+  "blue-gray": "bg-blue-gray-50 text-blue-gray-500 fill-blue-gray-500 stroke-blue-gray-500",
+  purple: "bg-purple-50 text-purple-500 fill-purple-500 stroke-purple-500",
+  teal: "bg-teal-50 text-teal-500 fill-teal-500 stroke-teal-500",
+  cyan: "bg-cyan-50 text-cyan-500 fill-cyan-500 stroke-cyan-500",
+  pink: "bg-pink-50 text-pink-500 fill-pink-500 stroke-pink-500",
 };
  
 const navListMenuItems = [
   {
     color: "blue",
-    icon: FlagIcon,
-    title: "About us",
-    description: "Learn about our story and our mission statement.",
+    icon: CpuIcon,
+    title: "CPU",
+    description: "Central Processing Unit",
   },
   {
     color: "orange",
-    icon: ChatBubbleOvalLeftIcon,
-    title: "Press",
-    description: "News and writings, press releases, and resources",
+    icon: MotherboardIcon,
+    title: "Motherboard",
+    description: "Your machine's backbone for components.",
   },
   {
     color: "green",
-    icon: UsersIcon,
+    icon: GpuIcon,
     title: (
       <div className="flex items-center gap-1">
-        Careers{" "}
+        GPU{" "}
         <Chip
           size="sm"
-          color="green"
+          color="light-green"
           variant="ghost"
-          value="We're hiring!"
+          value="New Products"
           className="capitalize"
         />
       </div>
     ),
-    description: "We are always looking for talented people. Join us!",
+    description: "Graphics Processing Unit",
   },
   {
     color: "blue-gray",
-    icon: FolderIcon,
-    title: "Legal",
-    description: "All the stuff that we dan from legal made us add.",
+    icon: RamIcon,
+    title: "RAM",
+    description: "Random Acceess Memory",
   },
   {
     color: "purple",
-    icon: RocketLaunchIcon,
-    title: "Products",
-    description: "Checkout our products that helps a startup running.",
+    icon: HardDriveIcon,
+    title: "Storage",
+    description: "Your data storage.",
   },
   {
     color: "teal",
-    icon: FaceSmileIcon,
-    title: "Icons",
-    description: "Set of beautiful icons that you can use in your project.",
+    icon: DisplayIcon,
+    title: "Display",
+    description: "Your monitor of choice.",
   },
   {
     color: "cyan",
-    icon: PuzzlePieceIcon,
-    title: "UI Kits",
-    description: "High quality UI Kits helps you to 2x faster.",
+    icon: CaseIcon,
+    title: "Case",
+    description: "The housing for your components.",
   },
   {
     color: "pink",
-    icon: GiftIcon,
-    title: "Open Source",
-    description: "List of all our open-source projects, it's all free.",
+    icon: FanIcon,
+    title: "Fans",
+    description: "Your cooling setup.",
   },
 ];
  
@@ -151,8 +151,8 @@ function NavListMenu() {
               selected={isMenuOpen || isMobileMenuOpen}
               onClick={() => setIsMobileMenuOpen((cur) => !cur)}
             >
-              <Square3Stack3DIcon className="h-[18px] w-[18px]" />
-              Resources
+              <CubeIcon className="h-[18px] w-[18px]" />
+              Components
               <ChevronDownIcon
                 strokeWidth={2.5}
                 className={`hidden h-3 w-3 transition-transform lg:block ${
@@ -189,9 +189,15 @@ function NavList() {
         color="blue-gray"
         className="font-normal"
       >
-        <ListItem className="flex items-center gap-2 py-2 pr-4">
-          <CubeTransparentIcon className="h-[18px] w-[18px]" />
-          Blocks
+        <ListItem 
+          className="flex items-center gap-2 py-2 pr-4 
+          data-[active=true]:bg-gradient-to-tr data-[active=true]:from-blue-600 data-[active=true]:to-blue-400
+          data-[active=true]:text-white data-[active=true]:shadow
+          " 
+          data-active={route().current('products.index')}
+        >
+          <HomeIcon className="h-[18px] w-[18px]" />
+          Home
         </ListItem>
       </Typography>
       <NavListMenu />
@@ -203,8 +209,20 @@ function NavList() {
         className="font-normal"
       >
         <ListItem className="flex items-center gap-2 py-2 pr-4">
-          <UserCircleIcon className="h-[18px] w-[18px]" />
-          Account
+          <AiOutlineLaptop />
+          Laptops
+        </ListItem>
+      </Typography>
+      <Typography
+        as="a"
+        href="#"
+        variant="small"
+        color="blue-gray"
+        className="font-normal"
+      >
+        <ListItem className="flex items-center gap-2 py-2 pr-4">
+          <AiOutlineDesktop />
+          Desktops
         </ListItem>
       </Typography>
     </List>
