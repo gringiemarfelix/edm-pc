af55a68e549f732a969c6d73ad4c2b592b778126
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-06-17 18:37:45 +0800

    refactor: added heading for products

--- resources/js/Pages/Products/Index.jsx
+++ resources/js/Pages/Products/Index.jsx
@@ -1,6 +1,7 @@
 import CustomCarousel from "@/Components/CustomCarousel"
 import Layout from "@/Layouts/Layout"
 import { Head } from "@inertiajs/react"
+import { Typography } from "@material-tailwind/react"
 
 const Index = () => {
   const navbarHeight = document.getElementById('navbar')?.offsetHeight
@@ -15,6 +16,12 @@ const Index = () => {
       >
         <CustomCarousel />
       </div>
+      <div className="bg-gradient-to-br from-blue-500 to to-blue-700 h-24">
+        Test
+      </div>
+      <Typography variant="h1" className="text-center my-3">New Products</Typography>
+      <Typography variant="h1" className="text-center my-3">Top-Rated Products</Typography>
+      <Typography variant="h1" className="text-center my-3">Top-Selling Products</Typography>
     </Layout>
   )
 }
