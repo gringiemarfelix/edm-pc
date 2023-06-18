a70a7efc0d9d1ce304ab67a2647269acfc84a362
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-06-18 22:34:26 +0800

    feat: calculat Layout min-height from navbar and footer

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,12 +1,21 @@
+import Footer from "@/Components/Footer"
 import TopNav from "@/Components/Nav/Navbar"
 
 const Layout = ({ children }) => {
+  const navbarHeight = document.getElementById('navbar')?.offsetHeight
+  const footerHeight = document.getElementById('footer')?.offsetHeight
+  
   return (
     <div className="bg-gray-50">
       <TopNav />
-      <div className="min-h-screen">
+      <div 
+        style={{
+          minHeight: `calc(100vh - (${navbarHeight}px + ${footerHeight}px))`
+        }}
+      >
         { children }
       </div>
+      <Footer />
     </div>
   )
 }
