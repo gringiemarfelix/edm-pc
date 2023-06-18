6be4f1197074ef0f73c2fd9884371e66c2cdae92
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-06-18 21:23:25 +0800

    refactor: layout bg and min-height

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -2,9 +2,11 @@ import TopNav from "@/Components/Nav/Navbar"
 
 const Layout = ({ children }) => {
   return (
-    <div className="bg-neutral-50">
+    <div className="bg-gray-50">
       <TopNav />
-      { children }
+      <div className="min-h-screen">
+        { children }
+      </div>
     </div>
   )
 }
