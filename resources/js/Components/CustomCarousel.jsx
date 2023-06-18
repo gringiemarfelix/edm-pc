fba64ce3615b67928a88e8f83318c8597e4c02d3
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-06-18 16:26:25 +0800

    refactor: increase autoplayDelay in CustomCarousel

--- resources/js/Components/CustomCarousel.jsx
+++ resources/js/Components/CustomCarousel.jsx
@@ -1,7 +1,7 @@
 import { Carousel, Typography, Button } from "@material-tailwind/react";
 const CustomCarousel = () => {
   return (
-    <Carousel autoplayDelay={2500} autoplay loop>
+    <Carousel autoplayDelay={3000} autoplay loop>
       <div className="relative h-full w-full">
         <img
           // src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
