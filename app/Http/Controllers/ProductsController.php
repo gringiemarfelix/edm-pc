2c457b62287d85ccdf973d43aff1dcb5a426950d
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-06-18 18:15:41 +0800

    feat: added payment methods data

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductsController extends Controller
 {
@@ -16,7 +17,7 @@ class ProductsController extends Controller
         ]);
 
         $data = [
-            
+            'payments' => $this->getPaymentMethods()
         ];
 
         if($request->wantsJson()){
@@ -37,5 +38,50 @@ class ProductsController extends Controller
         }
 
         return Inertia::render('Products/Show', $data);
+    }
+
+    private function getPaymentMethods()
+    {
+        $payments = [
+            [
+                'name' => 'Cards',
+                'items' => [
+                    asset('img/payments/visa.svg'),
+                    asset('img/payments/mastercard.svg'),
+                ]
+            ],
+            [
+                'name' => 'E-Wallets',
+                'items' => [
+                    asset('img/payments/gcash.png'),
+                    asset('img/payments/grab-pay.png'),
+                    asset('img/payments/maya.png'),
+                ]
+            ],
+            [
+                'name' => 'Online Banking',
+                'items' => [
+                    asset('img/payments/bpi.png'),
+                    asset('img/payments/unionbank.png'),
+                ]
+            ],
+            [
+                'name' => 'Buy Now, Pay Later',
+                'items' => [
+                    asset('img/payments/billease.svg')
+                ]
+            ],
+            [
+                'name' => 'Over-the-Counter',
+                'items' => [
+                    asset('img/payments/coins-ph.png'),
+                    asset('img/payments/7-eleven.png'),
+                    asset('img/payments/m-lhuillier.png'),
+                    asset('img/payments/cebuana-lhuillier.png'),
+                ]
+            ],
+        ];
+
+        return $payments;
     }
 }
