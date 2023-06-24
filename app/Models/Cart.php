c2ae2dda981b756a237ecacd9f0bff5f2b7b100b
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-06-24 21:27:59 +0800

    feat: eager load product in Cart model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'product_id'
+    ];
+
+    protected $with = [
+        'product',
+    ];
+
+    // Relationships
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
