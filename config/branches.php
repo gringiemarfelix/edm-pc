2c1070715c6b2f7a1eb50ec4319d7c26f0448da9
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-06-29 23:48:26 +0800

    config: store branches

---
+++ config/branches.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'main' => [
+        'coordinates' => [
+            'lat' => '14.68527440',
+            'lng' => '121.06441420'
+        ],
+        'address' => 'FEU Diliman, Quezon City, Metro Manila, Philippines'
+    ]
+];
