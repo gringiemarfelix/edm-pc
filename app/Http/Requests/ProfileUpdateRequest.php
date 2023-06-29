e19a3a46b0fc12acd6da5c0272b605016154beb3
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-06-29 15:26:48 +0800

    feat: phone validation

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,7 +18,7 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-            'phone' => ['required', 'max:10']
+            'phone' => ['required', 'numeric', 'min:10' ,'digits:10', 'regex:/^9[0-9]{9}$/', Rule::unique(User::class, 'phone')->ignore($this->user()->id)]
         ];
     }
 }
