1ae26067897e9481e43482c260c0f55bde64e003
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-02 19:22:27 +0800

    feat: added Order model relationships

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
@@ -26,6 +27,11 @@ class Order extends Model
     ];
 
     // Relationships
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
     public function lalamove(): HasOne
     {
