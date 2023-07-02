b17a9623b3554481e8d077537afb1f66d1ab6440
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-02 19:31:52 +0800

    feat: catch paymongo webhook errors

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -22,31 +22,36 @@ class WebhookController extends Controller
     {
         $paymentPayload = json_decode(json_encode($request->input('data.attributes.data')));
 
-        // Update Payment Status
-        $payment = Payment::find($paymentPayload->id);
-        $payment->status = $paymentPayload->attributes->status;
-        $payment->save();
-
-        // Update Order Status
-        $order = $payment->order;
-        $order->status = 'PLACED';
-        $order->save();
-
-        $user = $order->user;
-
-        $quotationResponse = $this->lalamove->quotations()->get($order->lalamove->quotation);
-
-        // Build Order
-        $sender = new Contact('EDM PC', '+639123456789', $quotationResponse->stops[0]->stopId);
-        $receiver = new Contact($user->name, "+63{$user->phone}", $quotationResponse->stops[1]->stopId);
-        $lalamoveOrder = new Order($quotationResponse->quotationId, $sender, [$receiver]);
-
-        $orderResponse = $this->lalamove->orders()->create($lalamoveOrder);
-
-        // Save Order ID to Database
-        $lalamoveRecord = $order->lalamove;
-        $lalamoveRecord->order = $orderResponse->orderId;
-        $lalamoveRecord->save();
+        try {
+            $payment = Payment::find($paymentPayload->id);
+            $payment->status = $paymentPayload->attributes->status;
+            $payment->save();
+    
+            // Update Order Status
+            $order = $payment->order;
+            $order->status = 'PLACED';
+            $order->save();
+    
+            $user = $order->user;
+    
+            $quotationResponse = $this->lalamove->quotations()->get($order->lalamove->quotation);
+    
+            // Build Order
+            $sender = new Contact('EDM PC', '+639123456789', $quotationResponse->stops[0]->stopId);
+            $receiver = new Contact($user->name, "+63{$user->phone}", $quotationResponse->stops[1]->stopId);
+            $lalamoveOrder = new Order($quotationResponse->quotationId, $sender, [$receiver]);
+    
+            $orderResponse = $this->lalamove->orders()->create($lalamoveOrder);
+    
+            // Save Order ID to Database
+            $lalamoveRecord = $order->lalamove;
+            $lalamoveRecord->order = $orderResponse->orderId;
+            $lalamoveRecord->save();
+        } catch (\Throwable $th) {
+            info('Something went wrong with Paymongo Webhook', [
+                'payload' => json_encode($request->all(), JSON_PRETTY_PRINT)
+            ]);
+        }
 
         return response('OK');
     }
