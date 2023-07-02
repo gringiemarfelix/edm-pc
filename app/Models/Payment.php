72cf0987595ba85f7e9186ce77ce9e6e404bb184
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-02 19:22:16 +0800

    feat: added Payment model relationships

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Payment extends Model
 {
@@ -14,4 +16,16 @@ class Payment extends Model
     protected $casts = [
         'id' => 'string'
     ];
+
+    // Relationships 
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function order(): HasOne
+    {
+        return $this->hasOne(Order::class, 'payment_id');
+    }
 }
