79e71e62df7528ceb498a94f3b0a4bf6883a5dc6
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-02 21:02:06 +0800

    feat: dashboard base

---
+++ resources/js/Pages/Admin/Index.jsx
@@ -0,0 +1,13 @@
+import AdminLayout from "@/Layouts/AdminLayout"
+
+const Index = () => {
+  return (
+    <AdminLayout>
+      <div className="h-full w-full flex items-center p-6">
+        Dashboard
+      </div>
+    </AdminLayout>
+  )
+}
+
+export default Index
