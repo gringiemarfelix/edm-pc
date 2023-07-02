0dafa6a112aefa64ebd13610b88fb0c0ebe5b94b
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-02 21:05:41 +0800

    refactor: add title on admin dashboard page

--- resources/js/Pages/Admin/Index.jsx
+++ resources/js/Pages/Admin/Index.jsx
@@ -1,8 +1,10 @@
 import AdminLayout from "@/Layouts/AdminLayout"
+import { Head } from "@inertiajs/react"
 
 const Index = () => {
   return (
     <AdminLayout>
+      <Head title="Dashboard" />
       <div className="h-full w-full flex items-center p-6">
         Dashboard
       </div>
