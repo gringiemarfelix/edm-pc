1e0e21a3b621eef9270eb45246e2ab572dd2b430
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-03 20:17:24 +0800

    refactor: CategoryRequest slug to be nullable

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -23,7 +23,7 @@ class CategoryRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string'],
-            'slug' => ['string']
+            'slug' => ['string', 'nullable']
         ];
     }
 }
