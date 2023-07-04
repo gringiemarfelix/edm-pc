13bd49a466d7f6ee83f306e5e4bfe139b49c9e37
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-04 23:54:24 +0800

    feat: Init All

---
+++ app/Console/Commands/Initialize.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class Initialize extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'init';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Initialize everything.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Artisan::call('init:admin');
+        Artisan::call('init:categories');
+        Artisan::call('init:reviews');
+    }
+}
