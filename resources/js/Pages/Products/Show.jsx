024b89413ccfd1077c555dcc3c509b09935878cd
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-05 02:05:56 +0800

    refactor: Product/Show display overview

--- resources/js/Pages/Products/Show.jsx
+++ resources/js/Pages/Products/Show.jsx
@@ -274,7 +274,7 @@ const Show = () => {
                 </TabsHeader>
                 <TabsBody className="bg-blue-gray-50 shadow-inner rounded-lg my-3 lg:ml-3 lg:my-0">
                   <TabPanel value="overview" className="py-2 text-blue-gray-900">
-                    Overview
+                    {product.overview}
                   </TabPanel>
                   <TabPanel value="description" className="py-2 text-blue-gray-900">
                     <Typography variant="paragraph" color="blue-gray" className="whitespace-pre-wrap">
