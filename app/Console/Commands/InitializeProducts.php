477c57582372f3d96de4f7e18b28efe9cb386960
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-05 00:03:09 +0800

    feat: Init Products

---
+++ app/Console/Commands/InitializeProducts.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Brand;
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Console\Command;
+
+class InitializeProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'init:products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Initialize Products';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $categories = Category::get();
+
+        foreach($categories as $category){
+            Product::factory(5, [
+                'category_id' => $category->id
+            ])
+            ->has(Brand::factory(), 'brand')
+            ->create();
+        }
+
+        $this->info('Products Initialization Complete');
+
+        return 0;
+    }
+}
