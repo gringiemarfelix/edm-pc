58a63690387ab76d8a273042545ff1646da89ad4
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-04 23:48:02 +0800

    refactor: CreateAdmin to InitializeAdmin

--- app/Console/Commands/InitializeAdmin.php
+++ app/Console/Commands/InitializeAdmin.php
@@ -7,14 +7,14 @@ use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Hash;
 
-class CreateAdmin extends Command
+class InitializeAdmin extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'create-admin';
+    protected $signature = 'init:admin';
 
     /**
      * The console command description.
