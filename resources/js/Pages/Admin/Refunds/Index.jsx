450555e02ad1545b9ba4647f0799fe2eb5d5629b
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-04 20:44:55 +0800

    feat: Admin refunds

--- resources/js/Pages/Admin/Refunds/Index.jsx
+++ resources/js/Pages/Admin/Refunds/Index.jsx
@@ -1,10 +1,22 @@
 import AdminLayout from "@/Layouts/AdminLayout"
 import { Head } from "@inertiajs/react"
 import { Card, Typography, } from "@material-tailwind/react"
+import { router } from "@inertiajs/react";
 
-const TABLE_HEAD = ["#", "Order #", "User","Reason", ""];
+const TABLE_HEAD = ["#", "Order #", "User", "Reason", ""];
 
 const Index = ({ refunds }) => {
+  const updateRefundStatus = (refund, status) => {
+    router.put(route('admin.refuds.update', {
+      refund: refund.id
+    }), {
+      status: status
+    }, {
+      preserveState: true,
+      preserveScroll: true,
+    })
+  }
+
   return (
     <AdminLayout>
       <Head title="Refunds" />
@@ -15,12 +27,88 @@ const Index = ({ refunds }) => {
           </Typography>
         </div>
         <div className="lg:m-6 h-full">
-          <Card>
-            <div className="w-full h-full flex items-center justify-center p-24">
-              <Typography variant="h6" color="gray">
-                No refunds found.
-              </Typography>
-            </div>
+          <Card className="overflow-scroll w-full">
+            <table className="w-full min-w-max table-auto text-left">
+              <thead>
+                <tr>
+                  {TABLE_HEAD.map((head) => (
+                    <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal leading-none opacity-70"
+                      >
+                        {head}
+                      </Typography>
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  refunds.map((refund, index) => (
+                    <tr key={refund.id} className="hover:bg-gray-50">
+                      <td className="p-4">
+                        <div className="flex items-center gap-3">
+                          <Typography variant="small" color="blue-gray" className="font-medium">
+                            {refund.id}
+                          </Typography>
+                        </div>
+                      </td>
+                      <td className="p-4">
+                        <div className="flex items-center gap-3 font-normal text-sm text-blue-gray-900">
+                          {refund.order_id}
+                        </div>
+                      </td>
+                      <td className="p-4">
+                        <div className="flex items-center gap-3">
+                          <Avatar
+                            src={`https://ui-avatars.com/api/?size=128&background=e3f2fd&name=${refund.user.name}`}
+                            alt={refund.user.name + ' image'}
+                            size="md"
+                            className="border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1"
+                            variant="rounded"
+                          />
+                          <Typography variant="small" color="blue-gray" className="font-normal">
+                            {refund.user.name}
+                          </Typography>
+                        </div>
+                      </td>
+                      <td className="p-4">
+                        <div className="flex items-center gap-3">
+                          <Avatar
+                            src={`https://ui-avatars.com/api/?size=128&background=e3f2fd&name=${refund.user.name}`}
+                            alt={refund.user.name + ' image'}
+                            size="md"
+                            className="border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1"
+                            variant="rounded"
+                          />
+                          <Typography variant="small" color="blue-gray" className="font-normal">
+                            {refund.user.name}
+                          </Typography>
+                        </div>
+                      </td>
+                      <td className="p-4">
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                          {refund.reason}
+                        </Typography>
+                      </td>
+                      <td className="p-4">
+                        <Select 
+                          label="Status"
+                          value={refund.status}
+                          onChange={e => updateRefundStatus(refund, e)}
+                        >
+                          <Option value="PENDING">PENDING</Option>
+                          <Option value="COMPLETE">COMPLETE</Option>
+                          <Option value="REJECTED">REJECTED</Option>
+                        </Select>
+                      </td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
           </Card>
         </div>
       </div>
