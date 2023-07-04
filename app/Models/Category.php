8071a844a42a287ad38c5739ca82a5ebcc818509
Gringiemar Felix <gringiemarfelix.dev@gmail.com>
2023-07-05 00:07:39 +0800

    fix: Category Product relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,6 +14,6 @@ class Category extends Model
 
     public function products(): HasMany
     {
-        return $this->hasMany(Product::class, 'brand_id');
+        return $this->hasMany(Product::class, 'category_id');
     }
 }
